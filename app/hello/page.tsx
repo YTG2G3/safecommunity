5dd6f169d1a87fe26356dcab27124c924b38327c
YTG2G3 <scratchlover05@gmail.com>
2023-08-25 00:28:18 -0700

    8/24

---
+++ app/hello/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <main>
+            <h1>Hello World</h1>
+            <p>This is a paragraph</p>
+        </main>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function Landing() {
     return (
         <main>Hello World</main>
     )
