b6fa5be058fad4c8335d8e03e7e4eba5dba530c7
YTG2G3 <scratchlover05@gmail.com>
2023-09-01 23:38:36 -0700

    auth

---
+++ lib/auth-options.ts
@@ -0,0 +1,13 @@
+import { AuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+const authOptions: AuthOptions = {
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET
+        }),
+    ],
+}
+
+export default authOptions;
